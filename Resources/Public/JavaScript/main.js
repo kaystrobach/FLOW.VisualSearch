bd1d895787472154148183ab4d129c6a76a01461
Kay Strobach <info@kay-strobach.de>
2015-04-22 07:57:02 +0200

    [TASK] better input

--- Resources/Public/JavaScript/main.js
+++ Resources/Public/JavaScript/main.js
@@ -1,35 +1,73 @@
 (function ( $ ) {
-    $.fn.advancedSearch = function(settings) {
-        var defaults = {
-            container: '#search-input-area'
-        };
-        var settings = $.extend( {}, defaults, settings );
+    $.fn.extend({
+        // A simple proxy for `selectRange` that sets the cursor position in an
+        // input field.
+        setCursorPosition: function(position) {
+            return this.each(function() {
+                return $(this).selectRange(position, position);
+            });
+        },
 
-        $(settings['container']).html('<div contenteditable="true" class="token-input" data-type="facet"></div>');
-
-        $(settings['container']).on('click', function() {
-            $(this).children('div[contenteditable]').first().focus();
-        });
-        $(settings['container']).children('div[contenteditable]').on('input', function() {
-            $(this).html($(this).text());
-        });
-        $(settings['container']).children('div[contenteditable]').on('keypress', function(event) {
-            if(event.which == 13) {
-                var text = $(this).text()
-                $(this).text('');
-
-                if($(this).attr('data-type') === 'facet') {
-                    $(this).before('<div class="token token-facet" data-value="">' + text + '</div>');
-                    $(this).attr('data-type', 'value');
-                } else {
-                    $(this).before('<div class="token token-value" data-facet="">' + text + '</div>');
-                    $(this).attr('data-type', 'facet');
+        // Cross-browser way to select text in an input field.
+        selectRange: function(start, end) {
+            return this.filter(':visible').each(function() {
+                if (this.setSelectionRange) { // FF/Webkit
+                    this.focus();
+                    this.setSelectionRange(start, end);
+                } else if (this.createTextRange) { // IE
+                    var range = this.createTextRange();
+                    range.collapse(true);
+                    range.moveEnd('character', end);
+                    range.moveStart('character', start);
+                    if (end - start >= 0) range.select();
                 }
+            });
+        },
 
-                event.preventDefault();
-            }
-        });
-    }
+        advancedSearch: function (settings) {
+            var defaults = {
+                container: '#search-input-area'
+            };
+            var settings = $.extend({}, defaults, settings);
+
+            // create needed elements
+            $(settings['container']).html('<div contenteditable="true" class="token-input" data-type="facet"></div>');
+
+            // focus on click
+            $(settings['container']).on('click', function () {
+                $(this).children('div[contenteditable]').last().focus();
+            });
+
+            // strip tags on enter
+            $(settings['container']).children('div[contenteditable]').on('input', function () {
+                // fix wrong character ordering due to a chrome bug
+                window.setTimeout(function() {
+                    var text = $(this).text();
+                    $(this).html(text);
+                }, 100);
+
+            });
+
+            // react on input and build visual facets
+            $(settings['container']).children('div[contenteditable]').on('keypress', function (event) {
+                if (event.which == 13) {
+                    var text = $(this).text()
+                    $(this).text('');
+
+                    if ($(this).attr('data-type') === 'facet') {
+                        $(this).before('<div class="token token-wrapper"><span class="btn btn-link btn-xs"><span class="glyphicon glyphicon-remove"></span></span><div class="token token-facet" data-value="">' + text + '</div></div>');
+                        $(this).attr('data-type', 'value');
+                    } else {
+                        $(this).prev().append('<div class="token token-value" data-facet="">' + text + '</div>');
+                        $(this).attr('data-type', 'facet');
+                    }
+
+                    event.preventDefault();
+                }
+            });
+
+        }
+    });
 })(jQuery);
 
 $( document ).ready(function() {
