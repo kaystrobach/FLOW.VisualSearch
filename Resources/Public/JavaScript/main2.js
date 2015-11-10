fa6ac5b5c35b12223bc6fe61cf2d5dee70b060e0
Kay Strobach <info@kay-strobach.de>
2015-11-10 19:47:11 +0100

    [TASK] new input method

--- Resources/Public/JavaScript/main2.js
+++ Resources/Public/JavaScript/main2.js
@@ -20,6 +20,10 @@
      */
     function initialize(element) {
         settings.container = element;
+        settings.formfield = $(element).find('.form-control');
+        settings.facetarea = $(element).find('.visual-search-facets');
+        settings.ajaxArea = '#search-result-area';
+        settings.loadingContent = '<span class="visual-search-loading-spinner">Loading</span>';
 
         $(element).on('click', '.label .glyphicon', function() {
             $(this).parent().remove();
@@ -31,24 +35,56 @@
         // bind basic autocomplete
         $(element).find('.form-control').autocomplete(
             {
-                source:['a', 'b', 'c'],
-                minLength: 0,
-                select: function(event, ui) {
-                    // todo call save method
-                }
+                source:[],
+                minLength: 0
             }
         ).focus(function(){
             $(this).trigger('keydown');
         });
 
-        $(element).find('.form-control').on(
+        $(settings.formfield).on(
             'keydown',
             function(event) {
-                if(event.which === $.ui.keyCode.ENTER) {
-                    // todo call save method
+                var text = $(settings.formfield).val();
+                if(event.which === 13) {
+                    if($(settings.formfield).autocomplete('option', 'disabled')) {
+                        setValue(text, text);
+                        storeQueryInSession();
+                    } else if(text == '') {
+                        $(this).closest('form').submit();
+                    }
+                    $(this).val('');
+                    event.preventDefault();
+                }
+                // backspace
+                if ((event.which == 8) && (text == '')) {
+                    addFacetAutocomplete(this);
+                    $(settings.facetarea).children().last().remove();
+                    $(settings.formfield).blur();
+                    $(settings.formfield).focus();
+                    window.setTimeout(function() {
+                        storeQueryInSession();
+                    }, 100)
+
                 }
             }
         );
+
+        $(element).find('.btn-clear').on(
+            'click',
+            function() {
+                $(settings.facetarea).children().remove();
+                storeQueryInSession();
+            }
+        );
+
+        window.setTimeout(
+            function() {
+                addFacetAutocomplete($(element).find('.form-control'));
+            },
+            300
+        );
+
     }
 
     /**
@@ -78,7 +114,6 @@
             },
             50
         );
-        console.log('send ' + settings.container + getTerm(settings.container));
     }
 
     /**
@@ -106,6 +141,23 @@
         return '<span class="label label-default"><span class="token token-facet" data-facet="' + value.facet + '">' + value.facetLabel + '</span>: <span class="token token-value" data-value="' + value.value + '">' + value.valueLabel + '</span> <span class="glyphicon glyphicon-remove"></span></span> ';
     }
 
+    /**
+     * sets the last value
+     * @param value
+     * @param label
+     */
+    function setValue(value, label) {
+        $(settings.facetarea).children().last().children('[data-value]').attr('data-value', value);
+        $(settings.facetarea).children().last().children('[data-value]').html(label)
+        $(settings.formfield).val('');
+
+    }
+
+    /**
+     * extracts the query from the dom
+     * @param element
+     * @returns {Array}
+     */
     function getTerm(element) {
         var query = [];
         $.each($(element).find('.label'), function(key, value) {
@@ -119,6 +171,110 @@
             )
         });
         return query;
+    }
+
+    function addFacetAutocomplete(element) {
+        if($(element).autocomplete('instance')) {
+            $(element).autocomplete('enable');
+        }
+        $(element).attr('data-type', 'facet');
+        $(element).autocomplete(
+            'option',
+            {
+                source: function(request, response) {
+                    //
+                    $.ajax({
+                        dataType: "json",
+                        url: $(settings['container']).attr('data-facetsAction'),
+                        data: {
+                            term: request.term,
+                            query: getTerm(settings.container)
+                        },
+                        success: function(data) {
+                            response(data);
+                        }
+                    });
+                },
+                select: function( event, ui ) {
+                    var value = {
+                        facetLabel:ui.item.label,
+                        facet:ui.item.value,
+                        valueLabel:'',
+                        value:''
+                    };
+                    $(settings.facetarea).append(getFacet(value));
+                    $(settings.formfield).text('');
+                    if(ui.item.configuration.freeInput) {
+                        addValueFreeText(element);
+                    } else {
+                        addValueAutocomplete(element);
+                    }
+                    window.setTimeout(function() {
+                        // @todo add handling for disabled autocomplete
+                        //if(ui.item.configuration.freeInput) {
+                        //    $(element).autofocus('disable');
+                        //}
+                        $(element).focus();
+                    }, 50);
+
+                    return false;
+                }
+            }
+        );
+    }
+
+    /**
+     * gets an element and add a value to a given facet
+     * @param element
+     */
+    function addValueAutocomplete(element) {
+        if($(element).autocomplete('instance')) {
+            $(element).autocomplete('enable');
+        }
+        $(element).attr('data-type', 'value');
+
+        $(element).autocomplete(
+            'option',
+            {
+                source: function(request, response) {
+                    $.ajax(
+                        {
+                            dataType: "json",
+                            url: $(settings['container']).attr('data-valueAction'),
+                            data: {
+                                facet: $(settings.facetarea).children().last().children('[data-facet]').attr('data-facet'),
+                                term: request.term,
+                                query: getTerm(settings.container)
+                            },
+                            success: function(data) {
+                                response(data);
+                            }
+                        }
+                    );
+                },
+                select: function (event, ui) {
+                    setValue(ui.item.value, ui.item.label);
+                    addFacetAutocomplete(element);
+                    window.setTimeout(
+                        function() {
+                            $(element).focus();
+                        },
+                        50
+                    );
+
+                    storeQueryInSession();
+                    return false;
+                }
+            }
+        );
+    }
+
+    function addValueFreeText(element) {
+        if($(element).autocomplete('instance')) {
+            $(element).autocomplete('disable');
+        }
+
+        $(element).attr('data-type', 'value');
     }
 
 
