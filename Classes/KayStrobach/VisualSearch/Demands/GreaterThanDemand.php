70fd5e597cc5f4e9baaa593e9285790c30ea23c6
Kay Strobach <github@kay-strobach.de>
2020-10-12 20:09:59 +0200

    Create GreaterThanDemand.php

---
+++ Classes/KayStrobach/VisualSearch/Demands/GreaterThanDemand.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace KayStrobach\VisualSearch\Demands;
+
+
+class GreaterThanDemand extends AbstractDemand
+{
+
+    /**
+     * @param mixed $value
+     * @return object
+     */
+    public function getDemands($value)
+    {
+        $subDemands = [];
+        foreach ($this->fields as $matchField) {
+            $subDemands[] = $this->query->greaterThan($matchField, $value);
+        }
+        return $this->query->logicalOr($subDemands);
+    }
+}
