6c44659a67e47d3d1847faebbd78cbbae1852287
Jonas Knobloch <jonas.knobloch@t-online.de>
2024-09-05 17:01:58 +0200

    Add name argument to query view helper

--- Classes/ViewHelpers/QueryViewHelper.php
+++ Classes/ViewHelpers/QueryViewHelper.php
@@ -12,11 +12,18 @@ class QueryViewHelper extends \Neos\FluidAdaptor\Core\ViewHelper\AbstractViewHel
      */
     protected $queryStorage;
 
+    public function initializeArguments(): void
+    {
+        parent::initializeArguments();
+
+        $this->registerArgument('name', 'string', '', true);
+    }
+
     public function render() {
         try {
-            return json_encode($this->queryStorage->getQuery("pokemon")->jsonSerialize2()); // TODO move into template
+            return json_encode($this->queryStorage->getQuery($this->arguments['name'])->jsonSerialize2()); // TODO move into template
         } catch (\Exception $e) {
-            return htmlspecialchars(json_encode(["error" => $e->getMessage()]));
+            return htmlspecialchars(json_encode(['error' => $e->getMessage()]));
         }
     }
 }
