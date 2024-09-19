eb2733505a1c474bad314c9953fa6e61a5034ee3
Jonas Knobloch <jonas.knobloch@t-online.de>
2024-09-19 20:19:18 +0200

    Add rudimentary build step

--- Resources/Public/App/Components/SearchFacet.js
+++ Resources/Public/App/Components/SearchFacet.js
@@ -1,4 +1,4 @@
-import {LitElement, html, css} from 'https://cdn.jsdelivr.net/gh/lit/dist@3/all/lit-all.min.js';
+import {LitElement, html, css} from 'lit';
 
 export class SearchFacet extends LitElement {
   static styles = css`
---
+++ Resources/Public/App/app.js
@@ -0,0 +1,3 @@
+import {VisualSearch} from "./Components/VisualSearch";
+import {SearchFacet} from "./Components/SearchFacet";
+import {DebugLog} from "./Components/DebugLog";
